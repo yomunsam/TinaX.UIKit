6255c421b33847df357101f8f10132dec52cbd8e
yomunsam <yomunsam@outlook.com>
2020-04-12 20:55:26 +0800

    精简API

--- Runtime/Scripts/Interfaces/IUIKit.cs
+++ Runtime/Scripts/Interfaces/IUIKit.cs
@@ -12,28 +12,32 @@ namespace TinaX.UIKit
     {
         void CloseUI(UIEntity entity, params object[] args);
         void CloseUI(string UIName, params object[] args);
+
+        // ------ UIName / Args 
         IUIEntity OpenUI(string UIName, params object[] args);
-        IUIEntity OpenUI(string UIName, OpenUIParam openUIParam, params object[] args);
-        IUIEntity OpenUI<T>(string UIName, params object[] args) where T : XBehaviour;
-        IUIEntity OpenUIWithBehaviourType(string UIName, Type xBehaviourType, params object[] args);
-        IUIEntity OpenUI(string UIName, XBehaviour behaviour, params object[] args);
         Task<IUIEntity> OpenUIAsync(string UIName, params object[] args);
         void OpenUIAsync(string UIName, Action<IUIEntity, XException> callback, params object[] args);
+
+
+        // ------ UIName / Behavioyr / Args
+        IUIEntity OpenUI(string UIName, XBehaviour behaviour, params object[] args);
+        Task<IUIEntity> OpenUIAsync(string UIName, XBehaviour behaviour, params object[] args);
+        void OpenUIAsync(string UIName, XBehaviour behaviour, Action<IUIEntity, XException> callback, params object[] args);
+
+
+        // ------ UIName, Params / Args
+        IUIEntity OpenUI(string UIName, OpenUIParam openUIParam, params object[] args);
         Task<IUIEntity> OpenUIAsync(string UIName, OpenUIParam openUIParam, params object[] args);
         void OpenUIAsync(string UIName, OpenUIParam openUIParam, Action<IUIEntity, XException> callback, params object[] args);
-        Task<IUIEntity> OpenUIAsync<T>(string UIName, params object[] args) where T : XBehaviour;
-        Task<IUIEntity> OpenUIWithBehaviourTypeAsync(string UIName, Type xBehaviourType, params object[] args);
-        Task<IUIEntity> OpenUIAsync(string UIName, XBehaviour behaviour, params object[] args);
-        void OpenUIWithBehaviourTypeAsync(string UIName, Type xBehaviourType, Action<IUIEntity, XException> callback, params object[] args);
-        void OpenUIAsync(string UIName, XBehaviour behaviour, Action<IUIEntity, XException> callback, params object[] args);
-        IUIEntity OpenUI<T>(string UIName, OpenUIParam openUIParam, params object[] args) where T : XBehaviour;
-        IUIEntity OpenUIWithBehaviourType(string UIName, Type xBehaviourType, OpenUIParam openUIParam, params object[] args);
+        
+
+        
+        // ------ UIName / Behaviour / Params /Args
         IUIEntity OpenUI(string UIName, XBehaviour behaviour, OpenUIParam openUIParam, params object[] args);
-        Task<IUIEntity> OpenUIAsync<T>(string UIName, OpenUIParam openUIParam, params object[] args) where T : XBehaviour;
-        Task<IUIEntity> OpenUIWithBehaviourTypeAsync(string UIName, Type xBehaviourType, OpenUIParam openUIParam, params object[] args);
         Task<IUIEntity> OpenUIAsync(string UIName, XBehaviour behaviour, OpenUIParam openUIParam, params object[] args);
-        void OpenUIWithBehaviourTypeAsync(string UIName, Type xBehaviourType, OpenUIParam openUIParam, Action<IUIEntity, XException> callback, params object[] args);
         void OpenUIAsync(string UIName, XBehaviour behaviour, OpenUIParam openUIParam, Action<IUIEntity, XException> callback, params object[] args);
+        
+        
         IUIEntity OpenUIWithParam(string UIName, OpenUIParam openUIParam, params object[] args);
         void OpenUIWithParamAsync(string UIName, OpenUIParam openUIParam, Action<IUIEntity, XException> callback, params object[] args);
     }
