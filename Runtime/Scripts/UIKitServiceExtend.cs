a64bef54caf1defe95d36c934eb6baa9e5ac3ee7
yomunsam <yomunsam@outlook.com>
2020-06-08 05:55:44 +0800

    6.6.2-preview ヾ(•ω•`)o
    [Add] Extend method for "TinaX.Core" IXCore.UseUIKit() to simplify "IXCore.RegisterServiceProvider(new UIKitProvider())" method.
    [Modify] Dependency "TinaX.Core"  upgrade to 6.6.2-preview from version 6.6.1-preview

---
+++ Runtime/Scripts/UIKitServiceExtend.cs
@@ -0,0 +1,13 @@
+﻿using TinaX.UIKit;
+
+namespace TinaX.Services
+{
+    public static class UIKitServiceExtend
+    {
+        public static IXCore UseUIKit(this IXCore core)
+        {
+            core.RegisterServiceProvider(new UIKitProvider());
+            return core;
+        }
+    }
+}
