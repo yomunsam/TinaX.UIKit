090321cd1c0811dda961b1bb39ca1e96e32c96f6
yomunsam <yomunsam@outlook.com>
2020-07-25 00:18:15 +0800

    fix bug

--- Editor/Scripts/ImportHandler.cs
+++ Editor/Scripts/ImportHandler.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sirenix.Utilities;
 using TinaX;
 using TinaX.IO;
 using TinaX.UIKit.Const;
