38809627811925b25158ab2e388e56b07af8c0f2
yomunsam <yomunsam@outlook.com>
2020-11-01 22:31:52 +0800

    👀

--- Runtime/Scripts/DataBinding/BindableProperty.cs
+++ Runtime/Scripts/DataBinding/BindableProperty.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace TinaX.UIKit.DataBinding
 {
-    public class BindableProperty<T>
+    public class BindableProperty<T> : IDisposable
     {
         public delegate void ValueChangedDelegate(T oldValue, T newValue);
         public ValueChangedDelegate ValueChanged;
 
         private T _value;
+        private bool disposedValue;
 
         public BindableProperty() { _value = default(T); }
         public BindableProperty(T defaultValue)
@@ -51,7 +53,36 @@ namespace TinaX.UIKit.DataBinding
             return (_value != null ? _value.ToString() : string.Empty);
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    //释放托管状态(托管对象)
+                }
 
+                // 释放未托管的资源(未托管的对象)并替代终结器
+                // 将大型字段设置为 null
+                ValueChanged = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        // ~BindableProperty()
+        // {
+        //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
 
